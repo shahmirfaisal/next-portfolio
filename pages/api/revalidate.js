05e8baa87d5be978cff9ef61909179fcb4c436b5
Shahmir Faisal <shahmir049@gmail.com>
2022-08-07 20:45:38 +0500

    added ondemand isr

---
+++ pages/api/revalidate.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: "Invalid Token!" });
+  }
+
+  if (req.method !== "POST") {
+    return res.status(400).json({ message: "Invalid Request Method!" });
+  }
+
+  try {
+    await res.revalidate("/");
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send("Error Revalidating!");
+  }
+}
